b9fb7b5e3c7ca942be25a18b163580a573af66c8
Anirudh gangwar <anirudhgangwar444@gmail.com>
2020-02-24 02:14:36 +0530

    deployment changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app= express()
 
 app.use(express.static(__dirname+'/dist/'))
 app.get('/**/*', (req, res) => {
-    res.render('../dist/index', {
+    res.render('index', {
       req,
       res 
     });
